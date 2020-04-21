1d5e2f2d01170f27f90dfb11a2c3b4dd56ba7837
leon <leon@hooc.top>
2020-04-21 20:35:43 +0800

    加入路由校验

--- src/App.js
+++ src/App.js
@@ -8,21 +8,77 @@ import SingleFoodDetails from './Page/SingleFoodDetails';
 import UserInformation from './Page/UserInformation';
 import GoodsPublish from './Page/GoodsPublish';
 import AddNewGoods from './Page/AddNewGoods';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
+import { message } from 'antd';
 // import { HashRouter, Route, Redirect } from 'react-router-dom';
 function App() {
+  const isLogin = sessionStorage.getItem('isLogin');
   return (
     <div className="App">
       <HashRouter>
         <Route path='/' component={Header}></Route>
         <Route path='/' exact component={Home} ></Route>
-        <Route path='/shoppingCart' exact component={ShoppingCart}></Route>
         <Route path='/forget' exact component={ForgetPassword}></Route>
         <Route path='/foodDetail/:gid' exact component={SingleFoodDetails}></Route>
-        <Route path='/me' exact component={UserInformation}></Route>
-        <Route path='/publish' exact component={GoodsPublish}></Route>
-        <Route path='/addNewGoods' exact component={AddNewGoods}></Route>
-        
+        {/* 判断是否登录 */}
+        <Route exact path="/shoppingCart" render={
+          () => {
+            if (isLogin) {
+              return (
+                <ShoppingCart />
+              )
+            } else {
+              message.warning('请登录后查看')
+              return (
+                <Redirect to="/" />
+              )
+            }
+          }} />
+
+        <Route exact path="/me" render={
+          () => {
+            if (isLogin) {
+              return (
+                <UserInformation />
+              )
+            } else {
+              message.warning('请登录后查看')
+              return (
+                <Redirect to="/" />
+              )
+            }
+          }} />
+
+        <Route exact path="/publish" render={
+          () => {
+            if (isLogin) {
+              return (
+                <GoodsPublish />
+              )
+            } else {
+              message.warning('请登录后查看')
+              return (
+                <Redirect to="/" />
+              )
+            }
+          }} />
+
+        <Route exact path="/addNewGoods" render={
+          () => {
+            if (isLogin) {
+              return (
+                <AddNewGoods />
+              )
+            } else {
+              message.warning('请登录后查看')
+              return (
+                <Redirect to="/" />
+              )
+            }
+          }} />
+
+
+
       </HashRouter>
     </div>
   );
